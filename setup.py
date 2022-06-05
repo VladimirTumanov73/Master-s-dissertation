29ff74d79a65c20a1ba10b85f690713d21931e46
VladimirTumanov73 <105007081+VladimirTumanov73@users.noreply.github.com>
2022-06-05 16:50:43 +0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name="module1",
+      version="0.1",
+      description="Creation of forest declarations",
+      author="Tumanov Vladimir",
+      author_email="tymanovvova73@gmail.com",
+      url="http",
+      requires=['pytest'],
+      setup_requires=['pytest-runner'],
+      packages=["module1"]
+      )
